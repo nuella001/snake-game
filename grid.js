8eaa0df0079225f9411b9792d02f71ef9fb1acb8
nuella001 <chiomamemmanuella@gmail.com>
2022-09-03 18:59:36 -0700

    initial commit

---
+++ grid.js
@@ -0,0 +1,15 @@
+const gridsize = 21
+
+export function randomGridPosition() {
+    return{
+        x: Math.floor(Math.random() * gridsize) + 1,
+        y: Math.floor(Math.random() * gridsize) + 1
+    }
+}
+
+export function outsideGrid(position) {
+    return (
+        position.x < 1 || position.x > gridsize ||
+        position.y < 1 || position.y > gridsize
+    )
+}
